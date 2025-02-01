d461bffeb0d9a35a367d25925dcbcfedb70cbafc
EBi <ebi.x2003x@gmail.com>
2025-02-01 20:43:05 +0330

    `Refactor checkBtn click event handler to include additional conditions and disable input fields when game is lost.`

--- script.js
+++ script.js
@@ -16,29 +16,32 @@ let againButton = $.querySelector(".again_box");
 
 checkBtn.addEventListener("click", function () {
   userNumber = Number(numEntry.value);
-  if (userNumber === myNumber) {
-    qBox.textContent = myNumber;
-    $.body.style.backgroundColor = "green";
-    status.textContent = "Correct. Finally!";
-    if (highScore < currentScore) {
-      highScore = currentScore;
-      sp_highScore.textContent = highScore;
-      numEntry.disabled = true;
+  if (currentScore) {
+    if (userNumber === myNumber) {
+      qBox.textContent = myNumber;
+      $.body.style.backgroundColor = "green";
+      status.textContent = "Correct. Finally!";
+      if (highScore < currentScore) {
+        highScore = currentScore;
+        sp_highScore.textContent = highScore;
+        numEntry.disabled = true;
+      }
+      return;
+    } else if (userNumber > myNumber) {
+      status.textContent = "too high";
+      scoreNum.textContent = --currentScore;
+    } else {
+      status.textContent = "too low";
+      scoreNum.textContent = --currentScore;
     }
-    return;
-  } else if (userNumber > myNumber) {
-    status.textContent = "too high";
-    scoreNum.textContent = --currentScore;
-  } else {
-    status.textContent = "too low";
-    scoreNum.textContent = --currentScore;
+    if (currentScore === 0) {
+      numEntry.disabled = true;
+      checkBtn.disabled = true;
+      status.textContent = "You lost the game";
+      $.body.style.backgroundColor = "red";
+    }
+    console.log(myNumber);
   }
-  if (currentScore === 0) {
-    numEntry.disabled = true;
-    status.textContent = "You lost the game";
-    $.body.style.backgroundColor = "red";
-  }
-  console.log(myNumber);
 });
 
 againButton.addEventListener("click", function () {
